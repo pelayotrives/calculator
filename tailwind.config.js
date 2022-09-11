26afa01ef4222f60a5b775298d04f828956c1701
Pelayo Trives <pelayotrives@gmail.com>
2022-09-11 19:36:28 +0200

    Initial commit. Tailwind files created.

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      rotate: {
+        '360': '360deg',
+      },
+      keyframes: {
+        zoom: {
+          '0%, 100%': { transform: 'scale(1.1)' },
+        },
+        microzoom: {
+          '0%, 100%': { transform: 'scale(1.05)' },
+        },
+        alternate: {
+          '50%': { transform: 'translateY(0px)' },
+          '100%': { transform: 'translateY(-75px)' },
+        },
+      },
+      animation: {
+        zoom: 'zoom 1.5s ease-in-out infinite',
+        microzoom: 'microzoom 1.5s ease-in-out infinite',
+        alternate: 'alternate 2s ease-in-out infinite alternate',
+      }
+    },
+    screens: {
+      'xsm':'350px',
+      'sm':'500px',
+      'xmd':'625px',
+      'md':'750px',
+      'xlg':'875px',
+      'lg':'1000px',
+      'xl':'1125px',
+      '2xl':'1250px',
+      '3xl':'1325px',
+      '4xl':'1450px',
+      '5xl':'1575px',
+      '6xl':'1700px',
+      '7xl':'1825px',
+      '8xl':'1950px',
+    }
+  },
+  plugins: [],
+}
